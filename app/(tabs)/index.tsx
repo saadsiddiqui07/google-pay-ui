721648027d5354bb667487ef34978bf8e18990b6
saadsiddiqui07 <saadsiddiqui927@gmail.com>
2025-12-19 16:30:32 +0530

    refactor code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import HomeHeader from "@/components/home-header";
 import React from "react";
 import { ScrollView, View } from "react-native";
-import { Card, Text, useTheme } from "react-native-paper";
+import { useTheme } from "react-native-paper";
 
 export default function HomeScreen() {
   const theme = useTheme();
@@ -10,19 +10,6 @@ export default function HomeScreen() {
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
       <ScrollView contentContainerStyle={{ padding: 16 }}>
         <HomeHeader />
-        <Card style={{ backgroundColor: theme.colors.surface }}>
-          <Card.Content style={{ alignItems: "center" }}>
-            <Text
-              variant="headlineMedium"
-              style={{ color: theme.colors.primary, fontWeight: "bold" }}
-            >
-              Home
-            </Text>
-            <Text variant="bodyLarge" style={{ marginTop: 8 }}>
-              Welcome to Google Pay
-            </Text>
-          </Card.Content>
-        </Card>
       </ScrollView>
     </View>
   );
